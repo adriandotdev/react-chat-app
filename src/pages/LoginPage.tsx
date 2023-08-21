c363f0c012a9c327f3d8f1e4d68e2c1a8bc91ec2
Adrian Nads <marcelonads@gmail.com>
2023-08-21 18:33:08 +0800

    ADDED react-hook-form
    ADDED validation for onBlur and onChange in username and password input field

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,24 +3,29 @@ import { useNavigate, NavLink } from 'react-router-dom';
 import axios from 'axios';
 import '../css/form.css';
 import { ChatContext } from '../App';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+interface ILoginInput {
+    username: string,
+    password: string
+}
 
 function LoginPage() {
+
+    const { register, handleSubmit, setError, formState: { errors }, getValues } = useForm<ILoginInput>();
 
     const { setLoggedInUser } = useContext(ChatContext);
 
     const navigate = useNavigate();
 
-    const [loginInfo, setLoginInfo] = useState<object>(() => ({ username: '', password: '' }));
     const [isLoading, setIsLoading] = useState<boolean>(() => false);
 
-    const login = async (e) => {
-
-        e.preventDefault();
+    const submit: SubmitHandler<ILoginInput> = async (data) => {
 
         setIsLoading(() => true);
 
         try {
-            const response = await axios.post('http://localhost:3001/api/auth/login', loginInfo, {
+            const response = await axios.post('http://localhost:3001/api/auth/login', data, {
                 withCredentials: true
             });
 
@@ -40,17 +45,42 @@ function LoginPage() {
         <main className='form-main-container container-fluid row justify-content-center align-items-center'>
             <div className="login-form-container">
                 <div className="">
-                    <h1>Login</h1>
-                    <form onSubmit={login} className='d-flex flex-column gap-3'>
+                    <h1 className='login-heading text-center'>Humble</h1>
+                    <form onSubmit={handleSubmit(submit)} className='d-flex flex-column gap-3'>
                         <div>
                             <label className='form-label' htmlFor="username">Username</label>
-                            <input onChange={(e) => setLoginInfo({ ...loginInfo, username: e.target.value })} className="form-control" type="text" name="username" id="username" />
+                            <input {...register('username', {
+                                required: 'Please provide a username',
+                                onBlur: (e) => {
+                                    if (e.currentTarget.value !== '') setError('username', { type: 'validate', message: '' });
+                                    else setError('username', { type: 'validate', message: 'Please provide your username' });
+
+                                    console.log(errors)
+                                },
+                                onChange: (e) => {
+                                    if (e.currentTarget.value !== '') setError('username', { type: 'validate', message: '' });
+                                    else setError('username', { type: 'validate', message: 'Please provide your username' });
+                                }
+                            })} className={`form-control ${errors.username?.message ? 'is-invalid' : getValues().username ? 'is-valid' : ''}`} type="text" name="username" id="username" />
                         </div>
                         <div>
                             <label className='form-label' htmlFor="password">Password</label>
-                            <input onChange={(e) => setLoginInfo({ ...loginInfo, password: e.target.value })} className="form-control" type="password" name="password" id="password" />
+                            <input {...register('password', {
+
+                                required: 'Please provide a password',
+                                onBlur: (e) => {
+                                    if (e.currentTarget.value !== '') setError('password', { type: 'validate', message: '' });
+                                    else setError('password', { type: 'validate', message: 'Please provide your password' });
+
+                                    console.log(errors)
+                                },
+                                onChange: (e) => {
+                                    if (e.currentTarget.value !== '') setError('password', { type: 'validate', message: '' });
+                                    else setError('password', { type: 'validate', message: 'Please provide your password' });
+                                }
+                            })} className={`form-control ${errors.password?.message ? 'is-invalid' : getValues().password ? 'is-valid' : ''}`} type="password" name="password" id="password" />
                         </div>
-                        <button className='btn btn-primary mt-3'>
+                        <button className='btn-login btn mt-3'>
                             {isLoading ? <div className="spinner-grow spinner-grow-sm" role="status">
                                 <span className="visually-hidden">Loading...</span>
                             </div> : <span>Login</span>}
@@ -58,7 +88,7 @@ function LoginPage() {
 
                     </form>
                     <p className="text-center mt-3 fw-bold"><small>Don't have an account? <span className="text-primary">
-                        <NavLink to="/signup">Sign Up </NavLink>
+                        <NavLink className='btn-signup-route' to="/signup">Sign Up</NavLink>
                     </span></small></p>
                 </div>
             </div>
