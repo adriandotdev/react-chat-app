feccda520619e2c361a69ea23e6adad0db88d8e1
Adrian Nads <marcelonads@gmail.com>
2023-08-26 04:45:52 +0800

    created PrivatePage.tsx

---
+++ src/pages/PrivatePage.tsx
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+import { Outlet, Navigate, useLoaderData } from 'react-router-dom';
+
+function PrivatePage() {
+
+    const isVerified = useLoaderData();
+
+    if (isVerified) {
+        return <Outlet />
+    }
+
+    return (
+        <Navigate to="/" replace={true} />
+    )
+}
+
+export const VerifyChatPage = async () => {
+
+    try {
+        const response = await axios.post('http://localhost:3001/api/auth/verify', {}, { withCredentials: true });
+
+        return response.data;
+    }
+    catch (err) {
+
+        return null;
+    }
+}
+
+export default PrivatePage
+
