703ea4eead270818cac6ba55ad1c1ec30f7c282f
Adrian Nads <marcelonads@gmail.com>
2023-08-26 11:12:42 +0800

    created interface for Register form

---
+++ src/interfaces/IRegister.ts
@@ -0,0 +1,8 @@
+export default interface IRegister {
+    givenName: string,
+    middleName: string,
+    lastName: string,
+    username: string,
+    password: string,
+    confirmPassword: string
+}
